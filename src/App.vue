11d544b82f13ed6691889578b087436df86cea14
Carlos Robert <crobertdmelo@gmail.com>
2024-01-23 17:33:39 -0300

    add global state and setup main components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,26 @@
-
 <template>
-  My App
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList :transactions="transactions" />
+    <AddTransaction />
+  </div>
 </template>
+
+<script setup>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+
+import { ref } from 'vue';
+
+const transactions = ref ([
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "Salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+]);
+</script>
